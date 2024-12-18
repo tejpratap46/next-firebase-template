3b587cf0a106895274d0d38676db3a6436662003
Abdullahi Abass <abassabdullahi4@gmail.com>
2024-12-18 09:42:48 +0100

    feat: add firebase config to env

--- firebase/init.ts
+++ firebase/init.ts
@@ -6,13 +6,13 @@ import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCf-Dhz9B0jEksdKa79VP-N21QsIyQRTgE",
-  authDomain: "template-project-e6ae5.firebaseapp.com",
-  projectId: "template-project-e6ae5",
-  storageBucket: "template-project-e6ae5.firebasestorage.app",
-  messagingSenderId: "625134750302",
-  appId: "1:625134750302:web:61e8fa696292def9ab82cf",
-  measurementId: "G-8YX3SVJKF8",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -24,8 +24,8 @@ const auth = getAuth(app);
 const firestore = getFirestore(app);
 const functions = getFunctions(app);
 
-// Connect to emulators if NEXT_PUBLIC_USE_EMULATOR is true
-if (process.env.NEXT_PUBLIC_USE_EMULATOR === "true") {
+// Connect to emulators if NEXT_PUBLIC_FIREBASE_USE_EMULATOR is true
+if (process.env.NEXT_PUBLIC_FIREBASE_USE_EMULATOR === "true") {
   // Emulator host and port configuration
   const emulatorHost = "localhost";
   const authPort = 9099;
