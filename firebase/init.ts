db7b2e352bff6e3e3328caabaf59591e535e494c
tejpratapsingh <tej.singh@catalyzesystems.com>
2025-01-05 01:48:20 +0530

    Updated firebase init

--- firebase/init.ts
+++ firebase/init.ts
@@ -6,13 +6,13 @@ import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGERCP_4UH7uCuCCiRiOzsLngafWiqcS4",
-  authDomain: "tps-files.firebaseapp.com",
-  projectId: "tps-files",
-  storageBucket: "tps-files.appspot.com",
-  messagingSenderId: "133341432267",
-  appId: "1:133341432267:web:d0d7f2e40ff8e7787c0e4e",
-  measurementId: "G-K94ER1N7S9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
