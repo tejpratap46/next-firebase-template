7d5e14b1d033e0a5d261e7bbbf87ae88ad219a43
tejpratapsingh <tej.singh@catalyzesystems.com>
2025-01-05 12:47:14 +0530

    Updated firebase init

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import {useRouter} from "next/navigation";
 import {useCurrentUser, useGoogleSignIn} from "@/firebase/auth";
 import {auth, firestore, getErrMsg} from "@/firebase/init";
 import React from "react";
+import {Button} from "@headlessui/react";
 
 export default function Home() {
     const router = useRouter();
@@ -28,7 +29,8 @@ export default function Home() {
             <div className="sticky top-0 h-screen flex flex-col items-center justify-center bg-green-400">
                 <h2 className="text-4xl">Sample Template</h2>
                 <p>Scroll Down</p>
-                <button onClick={handleSignIn}
+                <Button onClick={handleSignIn}
+                        disabled={isLoading}
                         className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2">
                     <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                          fill="currentColor" viewBox="0 0 18 19">
@@ -37,7 +39,7 @@ export default function Home() {
                               clipRule="evenodd"/>
                     </svg>
                     Sign in with Google
-                </button>
+                </Button>
             </div>
             <div className="sticky top-0 h-screen flex flex-col items-center justify-center bg-indigo-600 text-white">
                 <h2 className="text-4xl">The Second Title</h2>
